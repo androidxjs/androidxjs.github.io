0524048baa5dd719610843ec4228b62d3334b49b
xjs <xujingshuai@yiruikecorp.com>
2023-03-14 17:11:05 +0800

    demo

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "65450beb7bab81bf863f22801b9f899d",
-"index.html": "26b28ecb2f5295f0d972902b322d1c9f",
-"/": "26b28ecb2f5295f0d972902b322d1c9f",
-"main.dart.js": "3d958432044543779dd0760cc5fb1be7",
+"index.html": "2cf610ec1c15fe780f429976768bd1ca",
+"/": "2cf610ec1c15fe780f429976768bd1ca",
+"main.dart.js": "c726ff0a4aeb8d731c8f73f373e95619",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
